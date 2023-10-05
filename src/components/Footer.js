c89461299f3efc351d056ab8f151e630dfc2d355
Your Name <you@example.com>
2023-10-05 21:11:51 +0530

    Updates

--- src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-const Footer = () =>{
-    return(
-           <div className="div-footer">
-            <h1>E-comm Dashoard &copy;2023</h1>
-           </div>
-        );
-}
-export default Footer;
